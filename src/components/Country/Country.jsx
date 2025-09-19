c83d8ceed51dae274fc71063195bd2e8117e29f7
hellohabib <habibdigitalmail@gmail.com>
2025-09-19 19:28:56 +0600

    conditional CSS added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ({country}) => {
     console.log(country.currencies.currencies)
+
+    const [visited, setVisited] = useState(false);
+
+
+
     const handleVisited = () =>{
-        console.log("Button Clicked");
+        //setVisited(true);
+        //visited ? setVisited(false) : setVisited(true)
+        //setVisited(visited ? false : true)
+        setVisited(!visited)
+        //চার ভাবে করা হল, যেকোনটাই কাজ করবে
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited && "country-visited" }`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
@@ -15,7 +24,9 @@ const Country = ({country}) => {
             <p>Area: {country.area.area},   {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>            
             <p>Continent: {country.continents.continents}</p>
             {/* <p>Currencies: {country.currencies.currencies}</p> */}
-            <button onClick={handleVisited}>Visited</button>
+            <button onClick={handleVisited}>
+                {visited ? "Visited" : "Not Visited"}
+            </button>
         </div>
     );
 };
