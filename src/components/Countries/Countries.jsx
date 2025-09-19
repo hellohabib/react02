ea8660567797a5c291b50f492a829d0d60fc3186
hellohabib <habibdigitalmail@gmail.com>
2025-09-19 07:52:23 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,17 @@
+// const Countries = () =>{
+//     return(
+
+//     )
+// }
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h3>From Countries component</h3>
+        </div>
+    );
+};
+
+export default Countries;
